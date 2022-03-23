d3f656cbf845eaf5d08a561c687f12297801811e
Matteo Morganti <matteo99morganti@gmail.com>
2022-03-23 17:19:03 +0100

    push

--- db-university2.sql
+++ db-university2.sql
@@ -1,5 +1,5 @@
 
-    -- Query con GROUP BY
+    ---------------------------- Query con GROUP BY ---------------------------------------
 
 --  Contare quanti iscritti ci sono stati ogni anno
 SELECT COUNT(`students`.`id`) AS `iscrizioni_annuali`, YEAR(`enrolment_date`) AS `anno_scolastico`
@@ -22,7 +22,9 @@ FROM `degrees`
 GROUP BY `degrees`.`department_id`
 
 
-    -- Query con JOIN
+
+    ------------------------------ Query con JOIN ---------------------------------------
+
 
 -- Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
 SELECT `students`.`name`,`students`.`surname`,`students`.`date_of_birth`,`degrees`.`department_id`,`degrees`.`name`
@@ -43,3 +45,29 @@ SELECT `course_teacher`.`teacher_id`,`courses`.`name`,`courses`.`description`,`c
 FROM `course_teacher`
 JOIN `courses` ON `course_teacher`.`course_id` = `course_teacher`.`course_id`
 WHERE `course_teacher`.`teacher_id` = 44
+
+
+-- Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+SELECT `students`.`surname`, `students`.`name`, `degrees`.`name` AS corso_di_laurea, `departments`.`name`AS dipartimento
+FROM `students`
+JOIN `degrees` ON `students`.`degree_id` = `degrees`.`id`
+JOIN `departments` ON `degrees`.`department_id` = `departments`.`id`
+ORDER BY `students`.`surname`ASC, `students`.`name` ASC
+
+
+-- Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+SELECT `degrees`.`name`AS corso_di_laurea, `courses`.`name`AS corso, `teachers`.`name`AS nome_docente,  `teachers`.`surname`AS cognome_docente
+FROM `degrees`
+JOIN `courses` ON `courses`.`degree_id` = `degrees`.`id`
+JOIN `course_teacher` ON `course_teacher`.`course_id` = `courses`.`id`
+JOIN `teachers` ON `course_teacher`.`teacher_id` = `teachers`.`id`
+
+
+-- Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+SELECT DISTINCT `teachers`. *
+FROM `teachers`
+JOIN `course_teacher` ON `teachers`.`id` = `course_teacher`.`teacher_id`
+JOIN `courses` ON `course_teacher`.`course_id` = `courses`.`id`
+JOIN `degrees` ON `courses`.`degree_id` = `degrees`.`id`
+JOIN `departments`ON `degrees`.`department_id` = `departments`.`id`
+WHERE `departments`.`name` = "Dipartimento di Matematica"
